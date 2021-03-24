7c6f8e91395afcd658e35b88dc4906128d04c053
George <54204157+jorjyb0i@users.noreply.github.com>
2021-03-24 16:19:27 +0500

    Update TitledCollectionCell.swift

--- Viper/MainModule/TitledCollectionCell.swift
+++ Viper/MainModule/TitledCollectionCell.swift
@@ -28,7 +28,6 @@ class TitledCollectionCell: UICollectionViewCell {
         
         self.contentView.layer.addSublayer(gradient)
         
-//        mLabelTitle.textColor = .black
         mLabelTitle.numberOfLines = 3
         mLabelTitle.lineBreakMode = .byCharWrapping
         
